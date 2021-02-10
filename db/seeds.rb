bfedd4d7a62620a71c145779eab1264f51dc3353
satish <sathishkumar@techversantinfo.com>
2021-02-10 13:17:32 +0530

    Devise setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Admin user
+User.create(username: 'satish', email:"satish@example.com",password:"password",password_confirmation:"password",
+is_admin:true)
