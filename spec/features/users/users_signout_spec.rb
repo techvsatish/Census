dff2c735532cd50cdb1aeed2cc6ac3f5c8081df4
satish <sathishkumar@techversantinfo.com>
2021-02-12 14:05:33 +0530

    signout spec

--- spec/features/users/users_signout_spec.rb
+++ spec/features/users/users_signout_spec.rb
@@ -1,23 +1,22 @@
 require "rails_helper"
 
-RSpec.feature "Users Signin" do
+RSpec.feature "Users Signout" do
   before do
 		User.create(username: 'satish', email:"satish@example.com",password:"password",password_confirmation:"password",
     is_admin:true)
 	end
-  scenario "with invalid credentials" do
-    visit "/"    
-    fill_in "Email", with:"satish111@example.com"
-    fill_in "Password", with:"password"
-    click_button "Log in"
-    expect(page).to have_content("Invalid Email or password.")
-  end
-
-  scenario "with valid credentials" do
+  scenario "sign in and sign out user" do
     visit "/"    
     fill_in "Email", with:"satish@example.com"
     fill_in "Password", with:"password"
     click_button "Log in"
+    expect(page).to have_link 'Logout'
     expect(page).to have_content("Signed in successfully.")
+
+    click_link "Logout"
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
+
   end
+
+  
 end
